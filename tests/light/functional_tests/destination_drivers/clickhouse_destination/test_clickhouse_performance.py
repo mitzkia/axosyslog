ab382431b9d3d964f17a66c3325bd48acf36ea28
Andras Mitzki <andras.mitzki@axoflow.com>
2025-07-17 11:30:32 +0200

    foobar

--- tests/light/functional_tests/destination_drivers/clickhouse_destination/test_clickhouse_performance.py
+++ tests/light/functional_tests/destination_drivers/clickhouse_destination/test_clickhouse_performance.py
@@ -1,4 +1,6 @@
+import datetime
 import logging
+import time
 
 from axosyslog_light.common.blocking import wait_until_true_custom
 from axosyslog_light.helpers.loggen.loggen import LoggenStartParams
@@ -15,52 +17,93 @@ clickhouse_valid_options = {
     "workers": 1,
     "batch_lines": 50000,
     "batch_timeout": 50000,
+    "disk-buffer": {
+        # "disk-buf-size": 10000000,  # 10 MB
+        "mem-buf-length": 100000,      # 1000 messages in memory
+        "mem-buf-size": 1000000,     # 1 MB in memory
+    },
 }
 
 
-def test_clickhouse_destination_perf(config, syslog_ng, clickhouse_server, clickhouse_client, port_allocator, loggen):
-
-    network_source = config.create_network_source(
-        ip="localhost",
-        port=port_allocator(),
-        transport="tcp",
-    )
-    # generator_source = config.create_example_msg_generator_source(num=1, template=f'"{custom_input_msg}"')
+def test_clickhouse_destination_perf_generic_padd(config, syslog_ng, clickhouse_server, clickhouse_client, port_allocator, loggen):
+    network_source = config.create_network_source(ip="localhost", port=port_allocator(), transport="tcp", log_iw_size=10000, log_fetch_limit=1000)
     clickhouse_destination = config.create_clickhouse_destination(**clickhouse_valid_options)
     config.create_logpath(statements=[network_source, clickhouse_destination])
+
+    MSG_COUNTER = 2000000
 
     clickhouse_server.start()
     clickhouse_client.create_table("test_table", [("message", "String")])
 
-    # syslog_ng.start_params.stderr = False
     syslog_ng.start_params.trace = False
-    # syslog_ng.start_params.verbose = False
+    syslog_ng.start_params.debug = False
+    syslog_ng.start_params.verbose = False
 
     syslog_ng.start(config)
+    time.sleep(1)
+
     loggen.start(
         LoggenStartParams(
             target=network_source.options["ip"],
             port=network_source.options["port"],
             inet=True,
-            # rate=100000,
             perf=True,
             active_connections=1,
-            number=2000000,
+            number=MSG_COUNTER,
         ),
     )
 
-    wait_until_true_custom(lambda: loggen.get_sent_message_count() == 2000000, timeout=180)
+    logger.info("AAA-1: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert wait_until_true_custom(lambda: loggen.get_sent_message_count() == MSG_COUNTER, timeout=180)
+    logger.info("AAA-2: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert wait_until_true_custom(lambda: clickhouse_destination.get_stats()["written"] == MSG_COUNTER, timeout=180)
+    logger.info("AAA-3: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert clickhouse_client.run_query("SELECT COUNT(*) FROM test_table;") == MSG_COUNTER, f"Expected {MSG_COUNTER} messages"
+    logger.info("AAA-4: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    import time
-    time.sleep(20)  # Allow some time for the message to be processed
-    # assert clickhouse_destination.get_stats()["written"] >= 1
+    try:
+        syslog_ng.stop()
+    except Exception as e:
+        logger.error(f"Error stopping syslog-ng: {e}")
+        pass
 
-    aaa = clickhouse_client.run_query("SELECT COUNT(*) FROM test_table;")
-    logger.info(f"Query result: {aaa}")
-    assert aaa == 2000000, f"Expected 2000000 messages, but got {aaa}"
-    # db_logs = clickhouse_destination.read_logs()
-    # logger.info(db_logs)  # For debugging purposes, can be removed later
-    # assert len(db_logs) == 100000
+
+def test_clickhouse_destination_perf_complex_filterx(config, syslog_ng, clickhouse_server, clickhouse_client, port_allocator, loggen):
+    network_source = config.create_network_source(ip="localhost", port=port_allocator(), transport="tcp", log_iw_size=10000, log_fetch_limit=1000)
+    clickhouse_destination = config.create_clickhouse_destination(**clickhouse_valid_options)
+    config.create_logpath(statements=[network_source, clickhouse_destination])
+
+    MSG_COUNTER = 2000000
+
+    clickhouse_server.start()
+    clickhouse_client.create_table("test_table", [("message", "String")])
+
+    syslog_ng.start_params.trace = False
+    syslog_ng.start_params.debug = False
+    syslog_ng.start_params.verbose = False
+
+    syslog_ng.start(config)
+    time.sleep(1)
+
+    loggen.start(
+        LoggenStartParams(
+            target=network_source.options["ip"],
+            port=network_source.options["port"],
+            inet=True,
+            perf=True,
+            active_connections=1,
+            number=MSG_COUNTER,
+        ),
+    )
+
+    logger.info("AAA-1: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert wait_until_true_custom(lambda: loggen.get_sent_message_count() == MSG_COUNTER, timeout=180)
+    logger.info("AAA-2: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert wait_until_true_custom(lambda: clickhouse_destination.get_stats()["written"] == MSG_COUNTER, timeout=180)
+    logger.info("AAA-3: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    assert clickhouse_client.run_query("SELECT COUNT(*) FROM test_table;") == MSG_COUNTER, f"Expected {MSG_COUNTER} messages"
+    logger.info("AAA-4: actual datetime: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
     try:
         syslog_ng.stop()
     except Exception as e:
